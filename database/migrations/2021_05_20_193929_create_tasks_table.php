b622c13d91cc06d32caedb7eed1b63db5eec817f
misa <tennis_love_misaki@yahoo.co.jp>
2021-05-20 11:05:14 +0000

    kadaifirst

--- database/migrations/2021_05_20_193929_create_tasks_table.php
+++ database/migrations/2021_05_20_193929_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Tasks extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class Tasks extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('content');
+            $table->string('content'); 
             $table->timestamps();
         });
     }
